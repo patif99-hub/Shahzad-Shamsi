8deb16335a4f1c7b9be8b9c03819c8d28cd80267
patif99-hub <patif99@gmail.com>
2025-12-26 11:37:28 +0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,4 @@
-const API_URL = https://script.google.com/macros/s/AKfycbx5jjksqS5mzm7zZtcAjQ4yf1aG88T6qfl99PYO_4jKc48nCxJEeiivfp7ZjvhMSDRhgg/exec
-
+const API_URL = "https://script.google.com/macros/s/AKfycbx5jjksqS5mzm7zZtcAjQ4yf1aG88T6qfl99PYO_4jKc48nCxJEeiivfp7ZjvhMSDRhgg/exec"
 async function api(action, payload = {}) {
   const res = await fetch(API_URL, {
     method: "POST",
