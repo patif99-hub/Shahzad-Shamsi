5b7cb3346f545907c266389c8852cbe4209a7ec2
patif99-hub <patif99@gmail.com>
2025-12-26 11:41:17 +0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,8 +3,11 @@ const API_URL = "https://script.google.com/macros/s/AKfycbx5jjksqS5mzm7zZtcAjQ4y
 async function api(action, payload = {}) {
   const res = await fetch(API_URL, {
     method: "POST",
-    headers: { "Content-Type": "text/plain;charset=utf-8" },
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8"
+    },
     body: JSON.stringify({ action, ...payload })
   });
+
   return res.json();
 }
