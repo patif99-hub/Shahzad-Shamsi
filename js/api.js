5fd5b5ce326b30566475862c7360ef19c4f36d1f
patif99-hub <patif99@gmail.com>
2025-12-26 11:17:54 +0500

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,10 @@
+const API_URL = https://script.google.com/macros/s/AKfycbx5jjksqS5mzm7zZtcAjQ4yf1aG88T6qfl99PYO_4jKc48nCxJEeiivfp7ZjvhMSDRhgg/exec
+
+async function api(action, payload = {}) {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "text/plain;charset=utf-8" },
+    body: JSON.stringify({ action, ...payload })
+  });
+  return res.json();
+}
